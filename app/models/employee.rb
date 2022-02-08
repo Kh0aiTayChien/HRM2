6f006a93c42df75f5de106eed78df65c6d987875
BnK <chien.hcckt@gmail.com>
2022-02-08 10:27:12 +0700

    fix error new epl

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,12 +1,10 @@
 class Employee < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  belongs_to :department
-  belongs_to :project
+  belongs_to :department, optional: true
+  belongs_to :project, optional: true
   belongs_to :user
   has_many :projects, through: :employee_projects
   has_many :employee_projects
-  belongs_to :role
+  belongs_to :role, optional: true
   has_one :employee_file
   accepts_nested_attributes_for :employee_file
 end
